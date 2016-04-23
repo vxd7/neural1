684d95cdf7ab4bc058f1127125478ae943271a9c
vxd7 <sapka@darkstar.example.net>
2016-04-23 21:47:59 +0300

    Added more neuron functions. Error fixes.

--- neuron.cpp
+++ neuron.cpp
@@ -19,20 +19,56 @@ public:
 		row = inputsCount + 1;
 		col = 32;
 
-		nmin = 50;
+		nmin = 1;
+		weights[0] = nmin;
 
 		randomizeWeights();
 	}
 
 	void randomizeWeights()
 	{
+		//seed random pool
+		srand (static_cast <unsigned> (time(0)));
+
 		for(int i = 1; i < inputsCount; i++)
 		{
-			//seed random pool
-			srand (static_cast <unsigned> (time(0)));
 			//generates random weights for neuron's weights [-1...1]
 			weights[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX * 2 - 1);
 		}
+	}
+
+	/*Changes weights of the current neuron. 
+	 * Input:
+	 * int *input -- vector
+	 * d - differenece between out of the neuron and expected out
+	 * */
+	void changeWeights(int *input, int d)
+	{
+		for(int i = 0; i < inputsCount; i++)
+		{
+			weights[i] = d * input[i];
+		}
+	}
+
+	//Treshold function
+	float tfunc(float sig)
+	{
+		//Heavieside here!
+		if(sig >= 0.0) return 1.0;
+		else return 0.0;
+	}
+
+	//Summation function
+	float summate(int *inputs)
+	{
+		float signal = 0.0;
+
+		for(int i = 0; i < inputsCount; i++)
+		{
+			signal += weights[i] * inputs[i];	
+		}
+
+		return tfunc(signal);
 	}
 
 }
